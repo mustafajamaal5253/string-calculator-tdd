19b5cd89c1095d83859d2d22c7f17d49247aeda8
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 17:35:24 +0530

    Handle three numbers with simple for loop

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,7 +2,11 @@ export function add(numbers) {
   if (numbers === "") return 0;
 
   const nums = numbers.split(',');
-  if (nums.length === 1) return parseInt(nums[0]);
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += parseInt(nums[i]);
+  }
   
-  return parseInt(nums[0]) + parseInt(nums[1]);
+  return sum;
 }
