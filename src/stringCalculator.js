5a4a8ab59cf4d19ca3928d8c09466016b867d05f
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 19:17:01 +0530

    REFACTOR : extract validate, parse and delimiter logic into separate functions

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,30 +1,34 @@
-// src/stringCalculator.js
 export function add(numbers) {
   if (numbers === "") return 0;
+  
+  const { delimiter, numbersToCalculate } = getDelimiterAndNumbers(numbers);
+  const numArray = splitAndParseNumbers(numbersToCalculate, delimiter);
+  validateNegatives(numArray);
+  
+  return numArray.reduce((sum, num) => sum + num, 0);
+}
 
-  const { delimiter, numbersToSum } = parseInput(numbers);
-  const nums = numbersToSum.split(delimiter)
-    .map(num => parseInt(num));
+function getDelimiterAndNumbers(input) {
+  if (!input.startsWith("//")) {
+    return {
+      delimiter: /[,\n]/,
+      numbersToCalculate: input
+    };
+  }
+  
+  return {
+    delimiter: input[2],
+    numbersToCalculate: input.substring(input.indexOf("\n") + 1)
+  };
+}
 
-  const negatives = nums.filter(num => num < 0);
+function splitAndParseNumbers(str, delimiter) {
+  return str.split(delimiter).map(num => parseInt(num));
+}
+
+function validateNegatives(numbers) {
+  const negatives = numbers.filter(num => num < 0);
   if (negatives.length > 0) {
     throw new Error(`negatives not allowed: ${negatives.join(',')}`);
   }
-
-  return nums.reduce((sum, num) => sum + num, 0);
-}
-
-function parseInput(numbers) {
-  if (!numbers.startsWith("//")) {
-    return {
-      delimiter: /[,\n]/,
-      numbersToSum: numbers
-    };
-  }
-
-  const firstNewLine = numbers.indexOf("\n");
-  return {
-    delimiter: numbers[2],
-    numbersToSum: numbers.substring(firstNewLine + 1)
-  };
 }
