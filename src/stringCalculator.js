225d49428fe22897ec65ac049aee73c3ed1c7e42
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 19:09:57 +0530

    refactored the code to handle all cases - Delimiter or negative values or handling of new line

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,47 +1,30 @@
 // src/stringCalculator.js
 export function add(numbers) {
   if (numbers === "") return 0;
-  
-  let numbersToCalculate = numbers;
-  let delimiter = ',';
-  
-  // custom delimiter check
-  if (numbers.startsWith("//")) {
-    delimiter = numbers[2];
-    numbersToCalculate = numbers.substring(numbers.indexOf("\n") + 1);
+
+  const { delimiter, numbersToSum } = parseInput(numbers);
+  const nums = numbersToSum.split(delimiter)
+    .map(num => parseInt(num));
+
+  const negatives = nums.filter(num => num < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negatives not allowed: ${negatives.join(',')}`);
   }
-  
-  // Split string and convert to numbers
-  let nums = [];
-  if (delimiter === ',') {
-    nums = numbersToCalculate.split(/[,\n]/);
-  } else {
-    nums = numbersToCalculate.split(delimiter);
+
+  return nums.reduce((sum, num) => sum + num, 0);
+}
+
+function parseInput(numbers) {
+  if (!numbers.startsWith("//")) {
+    return {
+      delimiter: /[,\n]/,
+      numbersToSum: numbers
+    };
   }
-  
-  // Convert strings to numbers
-  let numArray = [];
-  for (let i = 0; i < nums.length; i++) {
-    numArray.push(parseInt(nums[i]));
-  }
-  
-  // Check for negatives
-  let negativeNums = [];
-  for (let i = 0; i < numArray.length; i++) {
-    if (numArray[i] < 0) {
-      negativeNums.push(numArray[i]);
-    }
-  }
-  
-  if (negativeNums.length > 0) {
-    throw new Error("negatives not allowed: " + negativeNums.join(','));
-  }
-  
-  // Calculate sum
-  let sum = 0;
-  for (let i = 0; i < numArray.length; i++) {
-    sum += numArray[i];
-  }
-  
-  return sum;
+
+  const firstNewLine = numbers.indexOf("\n");
+  return {
+    delimiter: numbers[2],
+    numbersToSum: numbers.substring(firstNewLine + 1)
+  };
 }
