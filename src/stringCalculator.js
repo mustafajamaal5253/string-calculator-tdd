4bf4ee260f9ec0176e579ede897e3c32e2378a92
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 17:42:35 +0530

    Handle newline delimiter between numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,8 @@
 export function add(numbers) {
   if (numbers === "") return 0;
 
-  const nums = numbers.split(',');
-  
+  // Split string on either commas or newlines using regex
+  const nums = numbers.split(/[,\n]/);
+
   return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
