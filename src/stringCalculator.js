da0aa6d9ca3e14b97115abe611c34e0ca4139666
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 17:36:56 +0530

    REFACTOR: Simplify sum calculation using reduce method

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,11 +2,6 @@ export function add(numbers) {
   if (numbers === "") return 0;
 
   const nums = numbers.split(',');
-
-  let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    sum += parseInt(nums[i]);
-  }
   
-  return sum;
+  return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
