a8cd71631a4622a28c4c7fb29eb82c2d31de802f
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 17:40:15 +0530

    number with new line - test should fail as its not handled

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -16,4 +16,8 @@ describe('String Calculator', () => {
   test('should return sum when three comma separated numbers are passed', () => {
     expect(add("1,2,3")).toBe(6);
   });
+
+  test('should handle new line between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
