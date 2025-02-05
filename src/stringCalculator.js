cba416d48dd709ae4fcaa10c8655a6249e37833f
mustafajamaal5253 <mustafajamaal5253@gmail.com>
2025-02-05 19:06:52 +0530

    simple implementation to throw error if negative number are there

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,22 +1,47 @@
+// src/stringCalculator.js
 export function add(numbers) {
   if (numbers === "") return 0;
   
-  const { delimiter, numbersToSum } = parseInput(numbers);
-  const nums = numbersToSum.split(delimiter);
-  return nums.reduce((sum, num) => sum + parseInt(num), 0);
-}
-
-function parseInput(numbers) {
-  if (!numbers.startsWith("//")) {
-    return {
-      delimiter: /[,\n]/,
-      numbersToSum: numbers
-    };
+  let numbersToCalculate = numbers;
+  let delimiter = ',';
+  
+  // custom delimiter check
+  if (numbers.startsWith("//")) {
+    delimiter = numbers[2];
+    numbersToCalculate = numbers.substring(numbers.indexOf("\n") + 1);
   }
-
-  const firstNewLine = numbers.indexOf("\n");
-  return {
-    delimiter: numbers[2],
-    numbersToSum: numbers.substring(firstNewLine + 1)
-  };
+  
+  // Split string and convert to numbers
+  let nums = [];
+  if (delimiter === ',') {
+    nums = numbersToCalculate.split(/[,\n]/);
+  } else {
+    nums = numbersToCalculate.split(delimiter);
+  }
+  
+  // Convert strings to numbers
+  let numArray = [];
+  for (let i = 0; i < nums.length; i++) {
+    numArray.push(parseInt(nums[i]));
+  }
+  
+  // Check for negatives
+  let negativeNums = [];
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] < 0) {
+      negativeNums.push(numArray[i]);
+    }
+  }
+  
+  if (negativeNums.length > 0) {
+    throw new Error("negatives not allowed: " + negativeNums.join(','));
+  }
+  
+  // Calculate sum
+  let sum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    sum += numArray[i];
+  }
+  
+  return sum;
 }
